86529c26e99db0a2990c2fec5202f38f55a88590
Aِl-Mi'raj <54112754+AnonMiraj@users.noreply.github.com>
2024-02-01 19:26:53 +0000

    small changes

--- hmmmmm.py
+++ hmmmmm.py
@@ -36,26 +36,25 @@ async def archive(ctx):
             message:discord.Message = message 
             data = data_extractor(str(message.content))
             print(data)
-            if any(data):
-                messages_data.append([message.author.id, message.content,str(message.created_at.isoformat()),data["streak"],data["days"],data["problems"],(data["today"])])
+            if data.get("streak")!=None :
+                messages_data.append([message.author.id,message.author.name, message.content,str(message.created_at.isoformat()),data["streak"],data["days"],data["problems"],(data["today"])])
         # TODO 
         filename = f"{channel.name}-archive.csv"
         with open(filename, "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
-            writer.writerow(["Author", "Message","Message date","streak","days","problems","practiced_day"]) 
+            writer.writerow(["Author_id","Author_name", "Message","Message date","streak","days","problems","practiced_day"]) 
 
             """
-            [Author                           Message                                   Message Date                                      streak                    days                problems             practiced_day
+            [Author_id                        Author_name                               message                                   message date                                      streak                    days                problems             practiced_day
             
-            [the discord id of the author ||  the raw string content of the message ||  the date of the message in [YYYY-MM-DD] format || the streak of the user || days of the user ||  problems he solved ||  how many he solved in that day
+            [the discord id of the author ||  the discord name of the author        ||  the raw string content of the message ||  the date of the message in [YYYY-MM-DD] format || the streak of the user || days of the user ||  problems he solved ||  how many he solved in that day
             """
 
             writer.writerows(messages_data)
 
-    await ctx.send(f"Channel archived. Check {filename}",ephemeral=True)
-# TODO on message even for that channle that adds to the csv
+    await ctx.send(f"Channel archived. Check {filename}")
+# TODO on message event for that channle that adds to the csv
 # and if an edit happend it also edit the csv 
 bot.run(os.getenv('TOKEN')) 
 
 
-# a bit messy. iam going to eat, good luck.
