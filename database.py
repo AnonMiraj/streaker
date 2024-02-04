fb842e59793dd4de0d84a2e05cc53623e3b7155c
AnonMiraj <nabilmalek48@gmail.com>
2024-02-04 20:33:28 +0000

    added a basic database still needs a lot of polish

---
+++ database.py
@@ -0,0 +1,161 @@
+
+import sqlite3
+
+
+def create_triggers(cur):
+    # Create trigger to update highest_streak
+    cur.execute("""CREATE TRIGGER IF NOT EXISTS update_highest_streak
+                    AFTER INSERT ON trainee_records
+                    BEGIN
+                        UPDATE trainees
+                        SET highest_streak = (
+                            SELECT MAX(streak) FROM trainee_records
+                            WHERE trainee_records.discord_id = trainees.discord_id
+                        )
+                        WHERE trainees.discord_id = NEW.discord_id;
+                    END
+                """)
+
+    # Create trigger to update current_streak
+    cur.execute("""CREATE TRIGGER IF NOT EXISTS update_current_streak
+                    AFTER INSERT ON trainee_records
+                    BEGIN
+                        UPDATE trainees
+                        SET current_streak = current_streak + 1
+                        WHERE trainees.discord_id = NEW.discord_id
+                        AND NEW.streak > 0;
+                    END
+                """)
+
+    # Create trigger to handle broken streak
+    cur.execute("""CREATE TRIGGER IF NOT EXISTS handle_broken_streak
+                    AFTER INSERT ON trainee_records
+                    BEGIN
+                        UPDATE trainees
+                        SET current_streak = 0
+                        WHERE trainees.discord_id = NEW.discord_id
+                        AND NEW.streak = 0;
+                    END
+                """)
+
+    # Create trigger to update total_days
+    cur.execute("""CREATE TRIGGER IF NOT EXISTS update_total_days
+                    AFTER INSERT ON trainee_records
+                    BEGIN
+                        UPDATE trainees
+                        SET total_days = total_days + 1
+                        WHERE trainees.discord_id = NEW.discord_id;
+                    END
+                """)
+
+    # Create trigger to update total_problems
+    cur.execute("""CREATE TRIGGER IF NOT EXISTS update_total_problems
+                    AFTER INSERT ON trainee_records
+                    BEGIN
+                        UPDATE trainees
+                        SET total_problems = total_problems + NEW.today_problems
+                        WHERE trainees.discord_id = NEW.discord_id;
+                    END
+                """)
+
+
+def create_tables_if_not_exist():
+    # Connect to the database
+    conn = sqlite3.connect('trainee.db')
+    cur = conn.cursor()
+
+    # Check if the tables exist
+    cur.execute(
+        '''SELECT count(name) FROM sqlite_master
+        WHERE type='table' AND name='trainees' ''')
+    trainees_exists = cur.fetchone()[0]
+
+    cur.execute(
+        '''SELECT count(name) FROM sqlite_master
+        WHERE type='table' AND name='trainee_records' ''')
+    trainee_records_exists = cur.fetchone()[0]
+
+    # If the tables don't exist, create them
+    if not trainees_exists:
+        cur.execute("""CREATE TABLE trainees (
+                    discord_id TEXT PRIMARY KEY,
+                    discord_name TEXT,
+                    highest_streak INTEGER,
+                    current_streak INTEGER,
+                    total_days INTEGER,
+                    total_problems INTEGER,
+                    UNIQUE (discord_id)
+
+        )""")
+
+    if not trainee_records_exists:
+        cur.execute("""CREATE TABLE trainee_records (
+                    discord_id TEXT ,
+                    discord_name TEXT,
+                    post_date TEXT,
+                    message TEXT,
+                    streak INTEGER,
+                    today_problems INTEGER,
+                    FOREIGN KEY (discord_id) REFERENCES trainees (discord_id),
+                    UNIQUE (discord_id, post_date)
+        )""")
+
+    if not (trainees_exists and trainee_records_exists):
+        create_triggers(cur)
+
+    conn.commit()
+    conn.close()
+
+
+def add_record(record_info):
+    """
+    Add a record to the trainee_records table.
+
+    Args:
+        record_info (list): A list containing the record information in the following order:
+                            [discord_id, discord_name,post_date, message, streak, today_problems]
+
+    Returns:
+        bool: True if the record is successfully added, False otherwise.
+    """
+    # Unpack record information
+    discord_id, discord_name, post_date, message, streak, today_problems = record_info
+
+    # Connect to the database
+    conn = sqlite3.connect('trainee.db')
+    cur = conn.cursor()
+
+    try:
+        # Check if discord_id is associated with a trainee, if not, add it
+        cur.execute(
+            "SELECT COUNT(*) FROM trainees WHERE discord_id = ?", (discord_id,))
+        if cur.fetchone()[0] == 0:
+            # If discord_id is not associated with a trainee, add it with default values
+            cur.execute("""INSERT INTO trainees (discord_id, discord_name, highest_streak, current_streak, total_days, total_problems)
+                           VALUES (?, ?, 0, 0, 0, 0)""", (discord_id, discord_name))
+            print(f"added {discord_name} to the database")
+
+        # Get the current date
+        if streak != 0:
+            cur.execute(
+                "SELECT MAX(post_date) FROM trainee_records WHERE discord_id = ?", (discord_id,))
+            post_date = cur.fetchone()[0]
+        # Insert the record into trainee_records table
+        cur.execute("""
+        INSERT INTO trainee_records
+        (discord_id, discord_name, post_date, message, streak, today_problems)
+         VALUES (?, ?, ?, ?, ?, ?)""",
+                    (discord_id, discord_name, post_date, message, streak, today_problems))
+        # print((discord_id, discord_name, post_date,
+        # message, streak, today_problems))
+
+        # Commit the transaction
+        conn.commit()
+
+        return True
+    except Exception as e:
+        print("Error adding record:", e)
+        conn.rollback()
+        return False
+    finally:
+        conn.close()
--- hmmmmm.py
+++ hmmmmm.py
@@ -1,60 +1,54 @@
 import discord
 import os
-import csv
 from dotenv import load_dotenv
-from funks import get_config,data_extractor
-
+from funks import data_extractor
+from database import add_record, create_tables_if_not_exist
 
 
 load_dotenv()
 bot = discord.Bot()
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
-@bot.event
-async def on_message(msg:discord.Message):
-    if get_config()["auto_mode"] == True:
-        # checks that the message in a channel the user wants to arhive 
-        if msg.channel.id in list(get_config()["channels"]): 
-            data:dict = data_extractor(msg.channel)
-            return data 
+# @bot.event
+# async def on_message(msg: discord.Message):
+#     if get_config()["auto_mode"] == True:
+#         # checks that the message in a channel the user wants to arhive
+#         if msg.channel.id in list(get_config()["channels"]):
+#             data: dict = data_extractor(msg.channel)
+#             return data
 
-    #TODO make this auto_mode thingy work
- 
-            
+    # TODO make this auto_mode thingy work
+
 
 @bot.slash_command()
 async def archive(ctx):
     channel = ctx.channel
-    
+
     async with ctx.typing():
         messages_data = []
         async for message in channel.history(limit=None):
-            message:discord.Message = message 
+            message: discord.Message = message
             data = data_extractor(str(message.content))
             print(data)
-            if data.get("streak")!=None :
-                messages_data.append([message.author.id,message.author.name, message.content,str(message.created_at.isoformat()),data["streak"],data["days"],data["problems"],(data["today"])])
-        # TODO 
-        filename = f"{channel.name}-archive.csv"
-        with open(filename, "w", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Author_id","Author_name", "Message","Message date","streak","days","problems","practiced_day"]) 
+            if data.get("streak") is not None:
+                messages_data.append([str(message.author.id), message.author.name,
+                                      str(message.created_at.isoformat()), message.content, data["streak"], data["today"]])
+
+        for record in messages_data:
+            add_record(record)
 
             """
-            [Author_id                        Author_name                               message                                   message date                                      streak                    days                problems             practiced_day
-            
+            [discord_id                       discord_name                             message                                   message date                                      streak                    days                problems             practiced_day
+
             [the discord id of the author ||  the discord name of the author        ||  the raw string content of the message ||  the date of the message in [YYYY-MM-DD] format || the streak of the user || days of the user ||  problems he solved ||  how many he solved in that day
             """
-
-            writer.writerows(messages_data)
-
-    await ctx.send(f"Channel archived. Check {filename}")
+    await ctx.send("Channel archived. Check database")
 # TODO on message event for that channle that adds to the csv
-# and if an edit happend it also edit the csv 
-bot.run(os.getenv('TOKEN')) 
-
-
+# and if an edit happend it also edit the csv
+create_tables_if_not_exist()
+bot.run(os.getenv('TOKEN'))
